ca4cc7a5efb2122786f43b0657f73842552245e2
PRADIP-1596 <9187pradipyadav@gmail.com>
2020-07-10 21:41:10 +0530

    [Pradip] Move for win condition

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -2,16 +2,17 @@
 
 flag=1
 declare -a board
+#EMPTY=''
 player=1
 system=0
 count=0
 #Printing Board
 boardPrint() {
-	echo "${board[0]} | ${board[1]} | ${board[2]}"
+	echo "${board[1]} | ${board[2]} | ${board[3]}"
 	echo "-------"
-	echo "${board[3]} | ${board[4]} | ${board[5]}"
+	echo "${board[4]} | ${board[5]} | ${board[6]}"
 	echo "-------"
-	echo "${board[6]} | ${board[7]} | ${board[8]}"
+	echo "${board[7]} | ${board[8]} | ${board[9]}"
 }
 #Toss for first play
 firstToss() {
@@ -46,74 +47,64 @@ symbolAssigning() {
 		fi
 	fi
 }
-firstToss
-symbolAssigning
 
-echo "system symbol = $systemSymbol"
-echo "player symbol = $playerSymbol"
-
+#checking winning condition
 winnigCheck() {
 		symbol=$1
 		winner=$2
-		if [[ ${board[0]} == $symbol && ${board[1]} == $symbol && ${board[2]} == $symbol ]]
+		if [[ ${board[1]} == $symbol && ${board[2]} == $symbol && ${board[3]} == $symbol ]]
 		then
-			((count2++))
-			echo "==================$Winner is winner================="
+			echo "==================$winner is winner================="
 			exit
-		elif [[ ${board[3]} == $symbol && ${board[4]} == $symbol && ${board[5]} == $symbol ]]
+		elif [[ ${board[4]} == $symbol && ${board[5]} == $symbol && ${board[6]} == $symbol ]]
 		then
-			((count2++))
-			echo "==================$Winner is winner================="
+			echo "==================$winner is winner================="
 			exit
-		elif [[ ${board[6]} == $symbol && ${board[7]} == $symbol && ${board[8]} == $symbol ]]
+		elif [[ ${board[7]} == $symbol && ${board[8]} == $symbol && ${board[9]} == $symbol ]]
 		then
-			((count2++))
-			echo "==================$Winner is winner================="
-			exit
-		elif [[ ${board[0]} == $symbol && ${board[3]} == $symbol && ${board[6]} == $symbol ]]
-		then
-			((count2++))
-			echo "==================$Winner is winner================="
+			echo "==================$winner is winner================="
 			exit
 		elif [[ ${board[1]} == $symbol && ${board[4]} == $symbol && ${board[7]} == $symbol ]]
 		then
-			((count2++))
-			echo "==================$Winner is winner================="
+			echo "==================$winner is winner================="
 			exit
 		elif [[ ${board[2]} == $symbol && ${board[5]} == $symbol && ${board[8]} == $symbol ]]
 		then
-			((count2++))
-			echo "==================$Winner is winner================="
+			echo "==================$winner is winner================="
 			exit
-		elif [[ ${board[0]} == $symbol && ${board[4]} == $symbol && ${board[8]} == $symbol ]]
+		elif [[ ${board[3]} == $symbol && ${board[6]} == $symbol && ${board[9]} == $symbol ]]
 		then
-			((count2++))
-			echo "==================$Winner is winner================="
+			echo "==================$winner is winner================="
 			exit
-		elif [[ ${board[2]} == $symbol && ${board[4]} == $symbol && ${board[6]} == $symbol ]]
+		elif [[ ${board[1]} == $symbol && ${board[5]} == $symbol && ${board[9]} == $symbol ]]
 		then
-			((count2++))
-			echo "==================$Winner is winner================="
+			echo "==================$winner is winner================="
+			exit
+		elif [[ ${board[3]} == $symbol && ${board[5]} == $symbol && ${board[7]} == $symbol ]]
+		then
+			echo "==================$winner is winner================="
 			exit
 		fi
 }
-
+#checking tie condition
 matchTie() {
-	for (( count = 0; count <= 8; count++ ))
+	for (( count=1;count<10;count++ ))
 	do
 		if [ -z "${board[count]}" ]
 		then
 			echo "match not tie"
-			count=9
-		elif (( $count ==  8 ))
-		then
-			echo "========= Match tie========"
-			exit
+			break
+		else
+			if (( $count == 9 ))
+			then
+				echo "========= Match tie========"
+				exit
+			fi
 		fi
 	done
 }
 
-
+#player playing game
 playerPlay() {
 	echo "=========== player chance =========="
 	win="player"
@@ -121,7 +112,7 @@ playerPlay() {
         read -p "please enter position where you want to put symbol: " playerPosition
 	if [ -z "${board[$playerPosition]}" ]
 	then
-		if (( $playerPosition >= 0 && $playerPosition <= 8 ))
+		if (( $playerPosition >= 1 && $playerPosition <= 9 ))
 		then
               		board[$playerPosition]=$playerSymbol
 			#symbol=$playerSymbol
@@ -139,10 +130,11 @@ playerPlay() {
 	fi
 }
 
-systemPlay() {
+#system playing game
+systemRandomPlay() {
 	echo "============= system chance =============="
 	win2="System"
-        systemPosition=$((RANDOM%8))
+        systemPosition=$((RANDOM%9+1))
 	if [ -z "${board[$systemPosition]}" ]
 	then
         	board[$systemPosition]=$systemSymbol
@@ -150,43 +142,70 @@ systemPlay() {
         	winnigCheck $systemSymbol $win2
 		matchTie
 	else
-		systemPlay
+		systemRandomPlay
 	fi
 	echo "============= system played =============="
 }
 
 
+systemWinCheck() {
+	win="system"
+	board[systemPosition]=$systemSymbol
+	winningCheck $SystemSymbol $win
+}
+
+
+systemPlay() {
+	for (( cellNumber=1;cellNumber<10;cellNumber++ ))
+	do
+		if [ -z "${board[$cellNumber]}" ]
+		then
+			board[$cellNumber]="$systemSymbol"
+			echo "system win check"
+			player="system"
+			winnigCheck $systemSymbol $player
+			board[$cellNumber]=""
+
+			if (( $cellNumber == 9 ))
+			then
+				echo "there is no cell for winning playing randon cell"
+				systemRandomPlay
+			fi
+		fi
+	done
+
+}
+
+
+
+#game started
 gameStart() {
 	if [ $toss -eq $player ]
 	then
 		while [ $flag -eq 1 ]
 		do
 			playerPlay
-			echo "-------------------------------------"
 			systemPlay
 		done
 	else
 		while [ $flag -eq 1  ]
 		do
 			systemPlay
-			echo "------------------------------------"
 			playerPlay
 		done
 	fi
 }
 
+echo "=========Main========"
+#toss function calling
+firstToss
+#symbolAssigining function calling
+symbolAssigning
+
+echo "system symbol = $systemSymbol"
+echo "player symbol = $playerSymbol"
+
+
+#gameStart function calling
 gameStart
-
-
-
-
-
-
-
-
-
-
-
-
-
 
